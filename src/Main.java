d4029440addfb50cf9ab04cd5185bae8728f6fd4
prayfortherain <prayfortherain@yandex.ru>
2023-05-10 06:40:09 +0300

    add message "there is no match"

--- src/Main.java
+++ src/Main.java
@@ -16,19 +16,18 @@ public class Main {
         catch(IOException ex){
             System.out.println(ex.getMessage());
         }
+        System.out.println("Enter the line:");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        //String input = "1.32.123.345.322.434.66.43.43.21.112.122.1214544.12.12.6544";
         Pattern pattern = Pattern.compile("((25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)");
         Matcher matcher = pattern.matcher(input);
         try(BufferedWriter bw = new BufferedWriter(new FileWriter("note.txt"))){
             while(matcher.find()){
-                bw.write(matcher.group() + '\n');
-            }
-        }
+                bw.write(matcher.group() + "\n"); }}
         catch (IOException ex){
             System.out.println(ex.getMessage());
         }
-
-    }
-}
+        if(new File("C://Users//acer//IdeaProjects//laba13", "note.txt").length() == 0) {
+            System.out.println("There is no match");
+        }
+    }}
